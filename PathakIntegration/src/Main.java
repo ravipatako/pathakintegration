0fb61eea00cf007a1805acbbb30ffa853e1f92e8
ravipathak527 <35974311+ravipathak527@users.noreply.github.com>
2019-02-05 14:50:12 -0500

    Work
    Added comments to the defintion -- more for the Integration project

--- PathakIntegration/src/Main.java
+++ PathakIntegration/src/Main.java
@@ -59,9 +59,26 @@ static class Excersise {
   }
 }
 
-//Variable is a location in memory
 
-//
+
+
+
+
+
+//Variable is a location in memory that contains data
+//Scope the whole class definition
+
+/* 
+ * Byte - Used for saving memory, can be used instead of Int
+ * Short - Used for saving memory where saving memory matters
+ * Int - Used for whole numbers
+ * Long - Used for really long numbers
+ * Float - Similar to a double it allows for decimal places, use a 'f' at the end
+ * Double - used for two decimal points
+ * Boolean - True or False statements
+ * Char - Single characters
+ * 
+ */
 
 
 }
