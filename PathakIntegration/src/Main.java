644696920623f81cad93929b986f0ee9331fe4f7
Ravi Pathak <rkpathak8189@eagle.fgcu.edu>
2019-02-05 15:36:41 -0500

    Inclass Work

--- PathakIntegration/src/Main.java
+++ PathakIntegration/src/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 // Ravi Pathak
 // A program integrating skills learned in COP 2006
 
-
 //changes to just for testing
 
 //Control (CMND (ON MAC)) Shift + F = Format
@@ -11,75 +10,76 @@ import java.util.Scanner;
 // when going from numbers to strong, clear buffer
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("Hello, to my Integration Project for COP 2006");
-    Introduction.main(args);
-    Excersise.Data();
-  }
+	public static void main(String[] args) {
+		System.out.println("Hello, to my Integration Project for COP 2006");
+		Introduction.main(args);
+		Excersise.Data();
 
-
-
- static class Introduction {
-	public static void main(String [] args) {
-		
-		//creating boolean values
-		boolean first = true;
-		boolean second = false; 
-		
-		//setting strings to the boolean value
-		String bLean = Boolean.toString(first);
-		String cLean = Boolean.toString(second);
-		
-		//printing out the string in the console to return the value
-		
-		System.out.println(bLean);
-		System.out.println(cLean);
-		
-		
 	}
-}
 
-static class Excersise {
-  public static void Data() {
-    int int1 = 2;
-    double double1 = 2.0;
-    String string1 = "2";
+	static class Introduction {
+		public static void main(String[] args) {
 
-    /* Declare and initialize second integer, double, and String variables. */
-    int int2 = 2;
-    double double2 = 2.0;
-    String string2 = "2";
+			// creating boolean values
+			boolean first = true;
+			boolean second = false;
 
-    /* Print the sum of both integer variables on a new line. */
-    System.out.println("The result of + with ints is " + (int1 + int2));
-    /* Print the sum of the double variables on a new line. */
-    System.out.println("The result of + with doubles is " + (double1 + double2));
-    /* Concatenate and print the String variables on a new line. */
-    System.out.println("The result of + with Strings is " + (string1 + string2));
-  }
-}
+			// setting strings to the boolean value
+			String bLean = Boolean.toString(first);
+			String cLean = Boolean.toString(second);
 
+			// printing out the string in the console to return the value
 
+			System.out.println(bLean);
+			System.out.println(cLean);
 
+		}
+	}
 
+	static class Excersise {
+		public static void Data() {
+			int int1 = 2;
+			double double1 = 2.0;
 
+			String string1 = "2";
 
+			/* Declare and initialize second integer, double, and String variables. */
+			int int2 = 2;
+			double double2 = 2.0;
+			String string2 = "2";
+
+			/* Print the sum of both integer variables on a new line. */
+			System.out.println("The result of + with ints is " + (int1 + int2));
+			/* Print the sum of the double variables on a new line. */
+			System.out.println("The result of + with doubles is " + (double1 + double2));
+			/* Concatenate and print the String variables on a new line. */
+			System.out.println("The result of + with Strings is " + (string1 + string2));
+		}
+	}
+
+	static class inClass {
+		double num1 = 5;
+		double num2 = 10;
+		// this is a call, in the paraenthesses is an argument
+		double squared = squaredIt(num1, num2, "Hello");
+
+		public static double squaredIt(double num1, double num2, String string) {
+			// TODO Auto-generated method stub
+			return num1 * num1;
+		}
+
+	}
 
 //Variable is a location in memory that contains data
 //Scope the whole class definition
 
-/* 
- * Byte - Used for saving memory, can be used instead of Int
- * Short - Used for saving memory where saving memory matters
- * Int - Used for whole numbers
- * Long - Used for really long numbers
- * Float - Similar to a double it allows for decimal places, use a 'f' at the end
- * Double - used for two decimal points
- * Boolean - True or False statements
- * Char - Single characters
- * 
- */
-
+	/*
+	 * Byte - Used for saving memory, can be used instead of Int Short - Used for
+	 * saving memory where saving memory matters Int - Used for whole numbers Long -
+	 * Used for really long numbers Float - Similar to a double it allows for
+	 * decimal places, use a 'f' at the end Double - used for two decimal points
+	 * Boolean - True or False statements Char - Single characters
+	 * 
+	 */
 
 }
-
