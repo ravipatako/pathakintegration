c9fa622dce96a130954e48458a5f3c6687058f70
Ravi Pathak <35974311+ravipathak527@users.noreply.github.com>
2019-02-28 14:31:41 -0500

    Add files via upload

---
+++ PathakIntegration/src/Taxes.java
@@ -0,0 +1,19 @@
+
+public class Taxes {
+	
+
+	public static double taxedMoney (double potentialPayment) {
+		double paycheck;
+			if (potentialPayment >= 0) {
+				
+			}
+			else {
+				
+			}
+		
+		return paycheck;
+	
+	
+	}
+
+}
