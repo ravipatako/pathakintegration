399f02397551b4cf1f958cef20a4655e3b5af17b
ravipathak527 <35974311+ravipathak527@users.noreply.github.com>
2019-04-13 01:31:28 -0400

    Friday night update

--- PathakIntegration/src/Split.java
+++ PathakIntegration/src/Split.java
@@ -4,10 +4,17 @@
 
 //This class is to help users work on their account for their preference when it comes to their savings
 
-public class Split {
+class Split {
 	/* Starting out with creating a class and then creating a private method
 	 *  The method gets the name and from that
 	 */
+	
+	protected String name;
+	private int deposit;
+	public int close;
+	private int min;
+	
+	
 	
 	private static void account(String name) {
 		
