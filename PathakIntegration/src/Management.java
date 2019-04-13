1db0b016820d7bcfb0b6a74c24fa1a0bbaa16636
ravipathak527 <35974311+ravipathak527@users.noreply.github.com>
2019-04-13 01:57:28 -0400

    Fixed

--- PathakIntegration/src/Management.java
+++ PathakIntegration/src/Management.java
@@ -4,7 +4,7 @@
 //This class is to help users work on their account for their preference when it comes to their savings
 public class Management {
 
-	public static void banking() {
+	private static void banking() {
 
 		account checking = new account();
 		account savings = new account();
@@ -21,6 +21,7 @@ class account {
 	 */
 
 	protected String name; // name of the account
+	private String owner; // name of the owner of the account
 	private double initialDeposit; // how much the user is depositing at first
 	public int yearsToClose; // estimated year when acocunt will be close
 
@@ -28,7 +29,6 @@ class account {
 	 * The years to close is used to determine some kind of interest that will be
 	 * collected throughout that period
 	 */
-
 	private int minimumStart; // minimum amount needed in the account
 
 }
