90e0b2d8b7bc73bff56c2ba62de3b1ff476859ea
ravipathak527 <35974311+ravipathak527@users.noreply.github.com>
2019-04-13 22:20:03 -0400

    PSI3

---
+++ PathakIntegration/src/integration.java
@@ -0,0 +1,26 @@
+// Ravi Pathak
+// A program integrating skills learned in COP 2006
+
+//changes to just for testing
+public class integration {
+	private int numTasks;
+	private double incomeMoney;
+	//setting private fields in the class
+	
+	//putting in a constructor without parameters
+	public integration() {
+		numTasks = 4;
+		incomeMoney = 23.56;
+	}
+	
+	 // The getter for the class
+	  public int getNumTasks() {
+	    return numTasks;
+	  }
+
+	  // Creating the setter for the class
+	  public void setIncomeMoney(double incomeMoney) {
+	    this.incomeMoney = 12.34 ;
+	  }
+	
+}
