2c60f9bee1524fc63c708aaf41369d28faaa4d69
Ravi <ravipathak527@gmail.com>
2019-04-15 14:04:13 -0400

    Update

--- PathakIntegration/src/Fixedrate.java
+++ PathakIntegration/src/Fixedrate.java
@@ -1,5 +1,12 @@
 // Ravi Pathak
 // A program integrating skills learned in COP 2006
-public class Fixedrate extends Loans  {
+public class Fixedrate extends Loans {
+	void approved() {
+		System.out.println("You were approved, for the fixed-rate.");
+	}
+
+	void declined() {
+		System.out.println("Sorry you were declined, work on that credit score.");
+	}
 
 }
--- PathakIntegration/src/Installment.java
+++ PathakIntegration/src/Installment.java
@@ -1,5 +1,12 @@
 // Ravi Pathak
 // A program integrating skills learned in COP 2006
 public class Installment extends Loans {
+	void approved() {
+		System.out.println("You were approved, for your loan!");
+	}
+
+	void declined() {
+		System.out.println("Sorry you were declined, for your loan. ");
+	}
 
 }
