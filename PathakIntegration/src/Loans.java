6f4b222988896d3d437b9423de4d0b44bc186aad
Ravi <ravipathak527@gmail.com>
2019-04-15 14:01:19 -0400

    Update Loans.java

--- PathakIntegration/src/Loans.java
+++ PathakIntegration/src/Loans.java
@@ -1,5 +1,11 @@
 // Ravi Pathak
 // A program integrating skills learned in COP 2006
 public class Loans {
+	void approved() {
+		System.out.println("You were approved");
+	}
 
+	void declined() {
+		System.out.println("Sorry you were declined");
+	}
 }
