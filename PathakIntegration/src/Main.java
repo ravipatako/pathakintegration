1c1aee74c0fbd5bff51e93c20b7ffde99c8db550
Ravi <ravipathak527@gmail.com>
2019-04-15 14:53:41 -0400

    work

--- PathakIntegration/src/Main.java
+++ PathakIntegration/src/Main.java
@@ -55,7 +55,7 @@ public class Main {
 		scan.nextLine();
 		// Getting input from the user telling how many categories they want their money
 		// sort
-		
+
 		Loans[] borrow = new Loans[5];
 		borrow[0] = new Installment();
 		borrow[1] = new Fixedrate();
@@ -63,10 +63,11 @@ public class Main {
 		borrow[3] = new Fixedrate();
 		borrow[4] = new Installment();
 		borrow[5] = new Installment();
-		
-		for (int x=0; x<5;++x) {
+
+		for (int x = 0; x < 5; ++x) {
 			borrow[x].approved();
 		}
+
 	}
 
 	// Variable is a location in memory that contains data
